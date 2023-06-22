c5655de758327590f8b024fd5cb5dbb57a5590d9
Marouane Ed-Dahraoui <dahraouimarouan@gmail.com>
2023-06-22 09:58:32 -0700

    A function that draws a straight line in the termina

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_line -  function that draws a straight line in the termina
+ *
+ * @n: variable type of integer
+*/
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+			break;
+		}
+		else
+		{
+			_putchar(45);
+		}
+	}
+	_putchar('\n');
+}
